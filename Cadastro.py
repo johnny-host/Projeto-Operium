dd748adb0a1f19864b8568e6205dfe31ebd1e36e
João Borges <borges.joao07@aluno.ifce.edu.br>
2026-08-31 20:18:49 -0300

    Sistema Cadastro Listas Funcionários

--- Cadastro.py
+++ Cadastro.py
@@ -1,15 +1,51 @@
+funcionarios = []
+
+continuar = "sim"
+
 print("=== CADASTRO FUNCIONÁRIO ===")
 
-nome = input("Nome: ")
-cargo = input("Cargo: ")
-salario = float(input("Salário: "))
-carga = int(input("Carga semanal: "))
-regime = input("Regime(6x1, 5x2, 12x36...): ")
+while continuar == "sim":
+    nome = input("Nome: ").lower()
+    cargo = input("Cargo: ").lower()
 
-print("=== FÚNCINÁRIO CADASTRADO ===")
-print(f"Nome: {nome}")
-print(f"Cargo: {cargo}")
-print(f"Salário: {salario}")
-print(f"Carga Semanal: {carga}h")
-print(f"Regime: {regime}")
-      
+    salario = float(input("Salário: "))
+    while salario <= 0:
+        print('Valor Inválido! Tente novamente.')
+        salario = float(input("Cadastre um valor válido!"))
+        
+    carga = int(input("Carga semanal: "))
+    while carga <= 0 or carga > 44:
+        print('Valor inválido ou não permitido. Tente novamante.')
+        carga = int(input("Digite a carga semanal: "))
+
+    regime = input("Regime: ").lower()
+    while regime not in ("6x1", "5x2", "12x36", "horista"):
+        print('Regime inválido! Tente novamente.')
+        regime = input("Regime (horista, 6x1, 5x2 ou 12x36):").lower()
+
+    funcionario = {
+        "nome": nome,
+        "cargo": cargo,
+        "salario": salario,
+        "carga": carga,
+        "regime": regime 
+    }
+
+    funcionarios.append(funcionario)
+
+    print("=== FÚNCINÁRIO CADASTRADO ===")
+    print(funcionario ["nome"])
+    print(funcionario ["cargo"])
+    print(f'R$ {funcionario["salario"]:.2f}')
+    print(funcionario ["carga"])
+    print(funcionario ["regime"])
+
+    continuar = input("Deseja cadastrar um novo funcionário? (sim/não)").lower()
+
+for funcionario in funcionarios:
+    print("\n=== FUNCIONÁRIOS ===")
+    print(funcionario["nome"])
+    print(funcionario ["cargo"])
+    print(f'R$ {funcionario["salario"]:.2f}')
+    print(funcionario ["carga"])
+    print(funcionario ["regime"])
