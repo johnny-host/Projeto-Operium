21967c19101a277b44b99d09c5f1ddcfb58a8213
João Borges <borges.joao07@aluno.ifce.edu.br>
2026-08-31 15:56:01 -0300

    Sistema de Cadastro Funcionário  (Básico)

---
+++ Cadastro.py
@@ -0,0 +1,15 @@
+print("=== CADASTRO FUNCIONÁRIO ===")
+
+nome = input("Nome: ")
+cargo = input("Cargo: ")
+salario = float(input("Salário: "))
+carga = int(input("Carga semanal: "))
+regime = input("Regime(6x1, 5x2, 12x36...): ")
+
+print("=== FÚNCINÁRIO CADASTRADO ===")
+print(f"Nome: {nome}")
+print(f"Cargo: {cargo}")
+print(f"Salário: {salario}")
+print(f"Carga Semanal: {carga}h")
+print(f"Regime: {regime}")
+      
